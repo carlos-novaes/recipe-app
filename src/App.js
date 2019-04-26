613d7f11eec510589fae03144ffbc125b707f96e
carlos-novaes <carlosnovaes@poli.ufrj.br>
2019-04-25 22:08:08 -0300

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
 import './App.css';
 
 class App extends Component {
+  getRecipe = e => {
+    e.preventDefault();
+    console.log('working!!');
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
